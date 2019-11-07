841ebe9a790a6597bdb165fdd6a42be402c7a754
Mark <markwalker@gatech.edu>
2019-11-06 20:48:36 -0500

    Current broken code of attempting to create a hierarchy

--- app/src/main/java/com/gatech/update/ui/home/HomeFragment.java
+++ app/src/main/java/com/gatech/update/ui/home/HomeFragment.java
@@ -1,24 +1,22 @@
 package com.gatech.update.ui.home;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -26,31 +24,33 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.gatech.update.Controller.CreateGroupActivity;
-import com.gatech.update.Controller.MainActivity;
 import com.gatech.update.R;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private ScrollView myGroups;
-    private Button desiredGroup;
+
+    // Relating to current CardView items
+    private CardView group_card;
+    private CardView.LayoutParams group_card_params;
+    private TextView group_name;
+    private TextView group_user_name;
+    private TextView group_user_status;
+
+    private ArrayList<String> groupNameList = new ArrayList<>();
+
     private LinearLayout ll;
-    private List<Type> myList = new ArrayList<>();
     private LinearLayout.LayoutParams params;
 
     private FirebaseUser mUser;
@@ -84,10 +84,17 @@ public class HomeFragment extends Fragment {
         // Get list of groups
         myGroups = root.findViewById(R.id.groupInfo);
         ll = root.findViewById(R.id.ll);
+
+        // Define layout params for each object
         params = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
         );
+        group_card_params = new CardView.LayoutParams(
+                CardView.LayoutParams.MATCH_PARENT,
+                CardView.LayoutParams.WRAP_CONTENT
+        );
+        group_card_params.setMargins(0, 0, 0,10);
 
         db.collection("Users")
                 .document(mUser.getUid())
@@ -95,14 +102,64 @@ public class HomeFragment extends Fragment {
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot doc : task.getResult()) {
+                    public void onComplete(@NonNull Task<QuerySnapshot> groupTask) {
+                        if (groupTask.isSuccessful()) {
+                            for (QueryDocumentSnapshot doc_group : groupTask.getResult()) {
+                                // We must dynamically populate home screen with group information
+                                // for every group user is a part of
+
+                                // 1. Create a new card view to contain current group
+                                group_card = new CardView(getContext());
+                                group_name = new TextView(getContext());
+                                group_name.setText(doc_group.getString("Group_Name"));
+
+                                // Editing params
+                                group_name.setTextSize(20);
+                                group_name.setTextColor(Color.WHITE);
+                                group_card.setCardBackgroundColor(Color.rgb(0, 133, 120));
+                                group_card.setContentPadding(8, 5, 8, 5);
+
+                                // Create the hierarchy
+                                // [ Linear Layout
+                                //    [ Card
+                                //       [ Group-specific info
+                                //   ...
+                                ll.addView(group_card, group_card_params);
+                                group_card.addView(group_name, params);
+
+                                // 2. Add Users
+                                db.collection("Groups")
+                                        .document(Objects.requireNonNull(doc_group.getString("Group_ID")))
+                                        .collection("Users")
+                                        .get()
+                                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<QuerySnapshot> userTask) {
+                                                if (userTask.isSuccessful()) {
+                                                    for (QueryDocumentSnapshot doc_user : userTask.getResult()) {
+                                                        // 2. Add Users
+                                                        group_user_name.setText(doc_user.getString("Display_Name"));
+                                                        group_card.addView(group_user_name, params);
+
+                                                        // 3. Add Status of each user
+//                                                        group_user_status.setText(db.collection("Users")
+//                                                                .document(doc_user.getString("Firebase_ID"))
+//                                                                .get()
+//                                                                .getResult().getString("Status"));
+                                                    }
+                                                    Log.d(TAG, "Success: Retrieved users.");
+                                                } else {
+                                                    Log.d(TAG, "Error retrieving user docs");
+                                                }
+                                            }
+                                        });
+
+
                                 // Dynamically adds buttons to the home screen of all groups user is
                                 // a part of
-                                desiredGroup = new Button(getContext());
-                                desiredGroup.setText(doc.getData().get("Group_Name").toString());
-                                ll.addView(desiredGroup, params);
+//                                desiredGroup = new Button(getContext());
+//                                desiredGroup.setText(doc_group.getData().get("Group_Name").toString());
+//                                ll.addView(desiredGroup, params);
 
                                 /* TODO: Display Information of each group (Names of those in each
                                          group & their status -> 2+ ways to implement)
@@ -113,9 +170,9 @@ public class HomeFragment extends Fragment {
                                  */
 
                             }
-                            Log.d(TAG, "Success: Retrieved: " + myList);
+                            Log.d(TAG, "Success: Retrieved groups.");
                         } else {
-                            Log.d(TAG, "Error retrieving docs.");
+                            Log.d(TAG, "Error retrieving group docs.");
                         }
                     }
                 });
